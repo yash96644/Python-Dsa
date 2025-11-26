eec80b94f2f07298ad027d4059d52709d670e755
yash96644 <yg095544@gmail.com>
2025-11-26 23:15:10 +0530

    leetcode 15 done

--- leetcode.py
+++ leetcode.py
@@ -211,3 +211,28 @@ res = []
 for num in nums:
     res.append(d[num])  
 return res
+
+
+# leetcode 15
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sym = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            'L' : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000,
+        }
+        num = 0
+        s = s.replace('IV' , 'IIII')
+        s = s.replace('IX' , 'VIIII')
+        s = s.replace('XL' , 'XXXX')
+        s = s.replace('XC' , 'LXXXX')
+        s = s.replace('CD' , 'CCCC')
+        s = s.replace('CM' , 'DCCCC')
+
+        for char in s:
+            num += sym[char]
+        return num
