af4c2bddceded29aeaf14d08132426b308c23487
yash96644 <yg095544@gmail.com>
2025-12-02 01:03:05 +0530

    leetcode 161

--- leetcode.py
+++ leetcode.py
@@ -265,3 +265,11 @@ for price in prices:
     else:
         maxProfit = max(maxProfit , price - minPorift)
     return maxProfit
+
+
+#leetcode 161
+
+n = len(nums)
+for i in range(len(nums)):
+    if ((i == 0 or nums[i] > nums[i-1] ) and (i==n -1 or nums[i] >  nums[i+1])):
+        return i    
