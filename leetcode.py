380d3931f0756d9786f625acbefb5147a5ee5dc5
yash96644 <yg095544@gmail.com>
2026-02-08 22:55:26 +0530

    leetcode 167

--- leetcode.py
+++ leetcode.py
@@ -283,3 +283,15 @@ for  i in range(1 , len(prices)):
     if prices[i] > prices[i-1]:
         profit += (prices[i] - prices[i-1])
 return profit
+
+# leetcode 167 
+
+left , right = 0 , len(numbers) - 1
+while left < right:
+    current_sum = numbers[left] + numbers[right]
+    if current_sum == target:
+        return [left + 1 , right + 1]
+    elif current_sum < target:
+        left += 1
+    else:
+        right -= 1
