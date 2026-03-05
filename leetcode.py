f25672d162bf24ae62963308c22ae9f558c4bdff
yash96644 <yg095544@gmail.com>
2026-03-06 00:26:40 +0530

    leetcode 125

--- leetcode.py
+++ leetcode.py
@@ -283,3 +283,24 @@ for  i in range(1 , len(prices)):
     if prices[i] > prices[i-1]:
         profit += (prices[i] - prices[i-1])
 return profit
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ''
+        
+        for ch in s:
+            if ch.isalnum():
+                res += ch.lower()
+
+        left = 0
+        right = len(res) - 1
+
+        while left < right:
+            if res[left] != res[right]:
+                return False
+            
+            left += 1
+            right -= 1
+
+        return True
