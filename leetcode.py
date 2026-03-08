7b054d4d0e87965ac22dd851142a329d1b5dc6df
yash96644 <yg095544@gmail.com>
2026-03-09 03:49:39 +0530

    leetcode 209

--- leetcode.py
+++ leetcode.py
@@ -310,4 +310,17 @@ class Solution:
             window_sum += arr[i] -  arr[i-k]
             max_sum = max(max_sum , window_sum)
         return max_sum
-        
+
+# leetcode 209 
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        current_sum = 0
+        min_length = 100010
+        for right in range(len(nums)):
+            current_sum += nums[right]
+            while current_sum >= target:
+                min_length = min(min_length , right - left + 1)
+                current_sum -= nums[left]
+                left += 1
+        return min_length if min_length != 100010 else 0
