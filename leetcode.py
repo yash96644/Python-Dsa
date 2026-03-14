9e569cca05cab51ef0f66d64ec2a0bfd4d0fd2ee
yash96644 <yg095544@gmail.com>
2026-03-15 01:14:08 +0530

    roman to interger done

--- leetcode.py
+++ leetcode.py
@@ -417,3 +417,27 @@ for char in t:
 if count1 == count2:
     print('vald')
 else: print('na')
+
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman= {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            'L' : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000,
+        }
+        totle = 0
+        prv = 0 
+        for i in reversed(s):
+            curr = roman[i]
+            if curr < prv:
+                totle -= curr
+            else:
+                totle += curr
+            prv = curr
+        print(totle)
